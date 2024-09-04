54c7c220babfdf88ae5bd762df89dc8e7613c642
DiegoRodriguez3D <69866476+DiegoRodriguez3D@users.noreply.github.com>
2024-09-04 10:58:43 +0200

    Initial Commit

---
+++ PokeDex Classic/PokeDex_ClassicApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDex_ClassicApp.swift
+//  PokeDex Classic
+//
+//  Created by Diego Rodriguez on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct PokeDex_ClassicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
